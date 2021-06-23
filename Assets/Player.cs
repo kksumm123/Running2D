88dff11a0817424a3c503f8071615895f6df5c70
kksumm123 <kksumm123@gmail.com>
2021-06-23 18:09:12 +0900

    velocity에 따른 애니메이션 구별

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,21 +9,35 @@ public class Player : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] Vector2 jumpForce = new Vector2(0, 1000f);
     [SerializeField] float gravityScale = 7f;
+    [SerializeField] float valueMidAirVeloY = 10f;
     void Start()
     {
         speed = RunGameManager.instance.speed;
         animator = GetComponentInChildren<Animator>();
         rigid = GetComponent<Rigidbody2D>();
         rigid.gravityScale = gravityScale;
+        animator.Play("Run");
     }
 
     void Update()
     {
         transform.Translate(speed * Time.deltaTime, 0, 0);
 
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
+        if (Input.GetKeyDown(KeyCode.Space))
             rigid.AddForce(jumpForce);
-        }
+
+        // 애니메이션
+        // 0보다 작으면 추락
+        // -10 ~ 10 = MidAir
+        // 0보다 크면 상승
+        float veloY = rigid.velocity.y;
+        if (Mathf.Abs(veloY) < valueMidAirVeloY) // MidAir
+            animator.Play("MidAir");
+        else if (veloY > 0)
+            animator.Play("JumpUp");
+        else if (veloY == 0)
+            animator.Play("Run");
+        else
+            animator.Play("JumpFall");
     }
 }
