34745ce0f407874d6b0ade5fbe4fc4560e73d723
kksumm123 <kksumm123@gmail.com>
2021-06-23 16:06:00 +0900

    카메라 무브 스크립트 추가

---
+++ Assets/MoveCam.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCam : MonoBehaviour
+{
+    [SerializeField] float speed = 20f;
+    void Update()
+    {
+        transform.Translate(speed * Time.deltaTime, 0, 0);
+    }
+}
