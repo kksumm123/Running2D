0200aa3d03534fe6d982a6811aac625c74ca0836
kksumm123 <kksumm123@gmail.com>
2021-06-25 19:25:18 +0900

    스크립트 작성 완료

--- Assets/MagneticAbility.cs
+++ Assets/MagneticAbility.cs
@@ -1,18 +1,31 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MagneticAbility : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // 자석에 끌린 Tr, 가속도
+    Dictionary<Transform, float> items = new Dictionary<Transform, float>();
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.transform.GetComponent<CoinItem>() == null)
+            return;
+        items.Add(collision.transform, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] float accelerate = 20f;
+    private void Update()
     {
-        
+        var pos = transform.position;
+
+        foreach (var item in items)
+        {
+            var coinTr = item.Key;
+            float acceleration = item.Value;
+            Vector2 dir = (pos - coinTr.position).normalized;
+            Vector2 move = dir * (acceleration + accelerate) * Time.deltaTime;
+            coinTr.Translate(move);
+        }
     }
 }
