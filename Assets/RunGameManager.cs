f1f346a3b3d4bcea341621cbc5ddb866f5c7612e
kksumm123 <kksumm123@gmail.com>
2021-06-25 17:02:21 +0900

    잘된다

--- Assets/RunGameManager.cs
+++ Assets/RunGameManager.cs
@@ -15,7 +15,7 @@ public class RunGameManager : MonoBehaviour
     }
     IEnumerator Start()
     {
-        timeText = transform.Find("TimeText").GetComponent<TextMeshProUGUI>();
+        timeText = transform.Find("Canvas/TimeText").GetComponent<TextMeshProUGUI>();
         for (int i = waitSeconds; i > 0; i--)
         {
             timeText.text = i.ToString();
@@ -23,6 +23,7 @@ public class RunGameManager : MonoBehaviour
         }
         timeText.text = "START!";
         yield return new WaitForSeconds(0.5f);
+        gameStateType = GameStateType.Playing;
         timeText.text = "";
     }
     public static bool IsPlaying()
