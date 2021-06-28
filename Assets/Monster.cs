183a01b8da3a03dcf29c9afbd0bbfa93063564b2
kksumm123 <kksumm123@gmail.com>
2021-06-28 21:20:47 +0900

    버섯몬스터 애니메이션 추가 hit, die 관련 스크립트 완료

---
+++ Assets/Monster.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+    }
+    [SerializeField] int hp = 10;
+    internal void OnDamage(int damage)
+    {
+        hp -= damage;
+        animator.Play("Hit");
+
+        if (hp <= 0)
+        {
+            StartCoroutine(DieCo());
+        }
+    }
+
+    [SerializeField] float dieDelay = 0.3f;
+    [SerializeField] float destroyDelay = 0.7f;
+
+    private IEnumerator DieCo()
+    {
+        yield return new WaitForSeconds(dieDelay);
+        animator.Play("Die");
+        yield return new WaitForSeconds(destroyDelay);
+        Destroy(gameObject);
+    }
+}
--- Assets/PlayerMeleeAttackCollider.cs
+++ Assets/PlayerMeleeAttackCollider.cs
@@ -1,18 +1,13 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMeleeAttackCollider : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int damage = 1;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
+        collision.GetComponent<Monster>()?.OnDamage(damage);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
