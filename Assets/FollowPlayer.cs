2d1b6a9cd5d24df9ed843b34a505bc89ee02c056
kksumm123 <kksumm123@gmail.com>
2021-06-28 19:08:32 +0900

    카메라에 followPlayer.cs 추가

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    Transform playerTr;
+    void Start()
+    {
+        playerTr = Player.instance.transform;
+    }
+    Vector3 newPos;
+    [SerializeField] float lerpValue = 0.01f;
+
+    void Update()
+    {
+        newPos = transform.position;
+        newPos.x = Mathf.Lerp(newPos.x, playerTr.position.x, lerpValue);
+        transform.position = newPos;
+    }
+}
