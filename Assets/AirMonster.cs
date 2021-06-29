38006a62cd04523d3e360222d397e5e300112d0e
kksumm123 <kksumm123@gmail.com>
2021-06-29 19:33:56 +0900

    길찾기 잘 한다, 바라보는 방향으로 바라보도록 수정

---
+++ Assets/AirMonster.cs
@@ -0,0 +1,25 @@
+﻿using Pathfinding;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirMonster : MonoBehaviour
+{
+    // 왼쪽, 오른쪽 진행방향에 따라 Rotation 하도록
+
+    AIPath aiPath;
+    IEnumerator Start()
+    {
+        aiPath = GetComponent<AIPath>();
+        while (true)
+        {
+            if (aiPath.desiredVelocity.x > 0)
+                transform.rotation = Quaternion.identity;
+            else if (aiPath.desiredVelocity.x < 0)
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+
+            yield return null;
+        }
+    }
+
+}
